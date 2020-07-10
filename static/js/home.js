9d6e161216d3af1c89b93316f13167cc04e4b5f0
Lorna Kamau <kamaulorna@gmail.com>
2020-07-10 12:15:56 +0300

    add site of the day section

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,20 @@
-function expand(){
-    $(this).toggleClass("on");
-    $(".menu").toggleClass("active");
-  };
-  $(".button").on('click', expand);
-  
+ /* NAVBAR */
+ $('.nav-toggle').on('click', function (e) {
+     $(this).toggleClass('nav-open');
+     $(".menu").toggleClass("active");
+ });
+
+ /* APP OF THE DAY */
+ $(function () {
+     $('.food > .info > .content .stars')
+         .bind('mousemove', function (e) {
+             var pct = e.pageX - $(this).offset().left;
+             pct = pct / $(this).width() * 100;
+             $(this).find('> em').css('width', pct + '%');
+         })
+         .bind('mouseleave', function () {
+             $(this).find('> em').animate({
+                 width: '91%'
+             }, 250);
+         });
+ });
