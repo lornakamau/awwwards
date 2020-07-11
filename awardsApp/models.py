8bc9c08360df6bc15eaeeb698c012082be5b6ea6
Lorna Kamau <kamaulorna@gmail.com>
2020-07-11 09:57:53 +0300

    add Profile model

--- awardsApp/models.py
+++ awardsApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_pic = CloudinaryField('image')
+    bio =  models.TextField()
+    location = models.CharField()
+    email = models.EmailField()
+    portfolio_link = models.URLField()
--- awardsApp/views.py
+++ awardsApp/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     date = dt.date.today()
-    return render(request, "user/profile.html", {"date": date})
+    return render(request, "home.html", {"date": date})
+
+def profile(request):
+    return render(request, "user/profile.html")
+
+def project(request):
+    return render(request, "project/project.html")
