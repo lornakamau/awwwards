3d415075b2ff48ec0b31f09473cebd11cae1f618
Lorna Kamau <kamaulorna@gmail.com>
2020-07-11 10:53:26 +0300

    test project class

--- awardsApp/tests.py
+++ awardsApp/tests.py
@@ -24,3 +24,42 @@ class ProfileTestClass(TestCase):
 
 
 
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
+        self.profile = Profile(user= self.lorna, profile_pic='mepng',bio='bio', location='Nairobi, Kenya', email='lorna@gmail', link='www.profile.com')
+        self.project = Project(name= "test", screenshot = "imageurl", description ="test project", link = "testlink", profile= self.profile)
+
+        self.lorna.save()
+        self.profile.save_profile()
+        self.project.save_project()
+
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_image_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_project(self):
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)> 0)
+
+    def test_delete_project(self):
+        projects1 = Project.objects.all()
+        self.assertEqual(len(projects1),1)
+        self.project.delete_project()
+        projects2 = Project.objects.all()
+        self.assertEqual(len(projects2),0)
+
+    def test_display_projects(self):
+        projects = Project.display_all_projects()
+        self.assertTrue(len(projects) > 0 )
+
+    def test_search_project(self):
+        project = Project.search_project('test')
+        self.assertEqual(len(project),1)
+
+    def test_get_user_projects_(self):
+        profile_projects = Project.get_user_projects(self.profile.id)
+        self.assertEqual(profile_projects[0].name, 'test')
+        self.assertEqual(len(profile_projects),1 )
