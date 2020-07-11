957f7a8eb849da43487369878392dad965cc1c52
Lorna Kamau <kamaulorna@gmail.com>
2020-07-11 10:15:44 +0300

    add Project model

--- awardsApp/models.py
+++ awardsApp/models.py
@@ -8,4 +8,23 @@ class Profile(models.Model):
     bio =  models.TextField()
     location = models.CharField()
     email = models.EmailField()
-    portfolio_link = models.URLField()
+    link = models.URLField()
+
+    def __str__(self):
+        return self.user.username
+
+class Project(models.Model):
+    name = models.CharField(max_length = 30)
+    screenshot = CloudinaryField('image')
+    description = models.TextField()
+    link = models.URLField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True, null = True)
+    voters = models.ManyToManyField(Profile, related_name="votes")
+    design_score = models.IntegerField(default=0)
+    usability_score = models.IntegerField(default=0)
+    content_score = models.IntegerField(default=0)
+    average_design = models.FloatField(default=0,)
+    average_usability = models.FloatField(default=0)
+    average_content = models.FloatField(default=0)
+    average_score = models.FloatField(default=0)
