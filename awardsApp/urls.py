818f44628e29b352e61ed4f0515eeb3b31c693ce
Lorna Kamau <kamaulorna@gmail.com>
2020-07-12 14:04:46 +0300

    add search form in navbar

--- awardsApp/urls.py
+++ awardsApp/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     url(r'^profile/$',views.profile,name = 'profile'),
     url(r'^project/$',views.project,name = 'project'),
-    url(r'^add-project/$',views.add_project,name = 'add_project'),
-    url(r'^rate-project/(?P<image_id>\d+)',views.rate_project,name = 'rate_project'),
+    url(r'^add_project/$',views.add_project,name = 'add_project'),
+    url(r'^rate_project/(?P<image_id>\d+)',views.rate_project,name = 'rate_project'),
+    url(r'^search_project/(?P<image_id>\d+)',views.search_project,name = 'search_project'),
 ]
