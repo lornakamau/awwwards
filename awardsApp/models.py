0b253cf4cc4a7f4072e62b2508c4d197f8112202
Lorna Kamau <kamaulorna@gmail.com>
2020-07-12 22:20:36 +0300

    add custom integer field

--- awardsApp/models.py
+++ awardsApp/models.py
@@ -66,13 +66,22 @@ class Project(models.Model):
     class Meta:
         ordering = ['-post_date']
     
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
 class Vote(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     project = models.ForeignKey(Project, on_delete= models.CASCADE, related_name = "votes")
     voter = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    design = models.IntegerField(default=0)
-    usability = models.IntegerField(default=0)
-    content = models.IntegerField(default=0)
+    design = fields.IntegerRangeField(min_value=1, max_value=10)
+    usability = fields.IntegerRangeField(min_value=1, max_value=10)
+    content = fields.IntegerRangeField(min_value=1, max_value=10)
 
     def save_vote(self):
         self.save()
