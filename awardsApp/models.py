0b76863fe134becba50261b68776125800ea77df
Lorna Kamau <kamaulorna@gmail.com>
2020-07-12 22:06:45 +0300

    add vote model tests

--- awardsApp/models.py
+++ awardsApp/models.py
@@ -69,7 +69,7 @@ class Project(models.Model):
 class Vote(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     project = models.ForeignKey(Project, on_delete= models.CASCADE, related_name = "votes")
-    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    voter = models.ForeignKey(Profile, on_delete=models.CASCADE)
     design = models.IntegerField(default=0)
     usability = models.IntegerField(default=0)
     content = models.IntegerField(default=0)
--- awardsApp/tests.py
+++ awardsApp/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile, Project
+from .models import Profile, Project, Vote
 
 class ProfileTestClass(TestCase):
     def setUp(self):
@@ -32,6 +32,8 @@ class ProjectTestClass(TestCase):
         self.project.save_project()
 
     def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
         Project.objects.all().delete()
 
     def test_image_instance(self):
@@ -60,3 +62,45 @@ class ProjectTestClass(TestCase):
         profile_projects = Project.get_user_projects(self.profile.id)
         self.assertEqual(profile_projects[0].name, 'test')
         self.assertEqual(len(profile_projects),1 )
+
+class VoteTestClass(TestCase):
+    def setUp(self):
+        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
+        self.profile = Profile(user= self.lorna, profile_pic='mepng',bio='bio', location='Nairobi, Kenya', email='lorna@gmail', link='www.profile.com')
+        self.project = Project(name= "test", screenshot = "imageurl", description ="test project", link = "testlink", profile= self.profile)
+        self.vote = Vote(voter=self.profile, project=self.project, usability= 4, design= 7, content = 3)
+
+        self.lorna.save()
+        self.profile.save_profile()
+        self.project.save_project()
+        self.vote.save_vote()
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        Project.objects.all().delete()
+        Vote.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.vote, Vote))
+
+    def test_save_vote(self):
+        votes = Vote.objects.all()
+        self.assertTrue(len(votes)> 0)
+
+    def test_delete_vote(self):
+        votes1 = Vote.objects.all()
+        self.assertEqual(len(votes1),1)
+        self.vote.delete_vote()
+        votes2 = Vote.objects.all()
+        self.assertEqual(len(votes2),0)
+
+    def test_get_project_voters(self):
+        voters = Vote.get_project_voters(self.profile)
+        self.assertEqual(voters[0].voter.user.username, 'lorna')
+        self.assertEqual(len(voters), 1)
+
+    def test_get_project_votes(self):
+        votes = Vote.get_project_votes(self.project)
+        self.assertEqual(votes[0].design, 7)
+        self.assertEqual(len(votes), 1)
